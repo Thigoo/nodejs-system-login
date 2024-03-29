61ce5f42b6b051ca1274bfbfbbf07d7b43a0d43c
Thigo Dev <thiagomvk08@gmail.com>
2024-03-29 12:11:22 -0300

    Creating database connection and user model

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//importing modules
+const {Sequelize, DataTypes} = require('sequelize');
+
+//Database connection with dialect of postgres specifying the database we are using
+//port for my database is 5432
+//database name is system-login
+const sequelize = new Sequelize(`postgres://postgres:123456@localhost:5432/system-login`, {dialect: 'postgres'});
+
+//checking if connection is done
+sequelize.authenticate()
+    .then(() => {
+        console.log(`Database connected to system-login`);
+    })
+    .catch((err) => {
+        console.log('Error connecting database, ', err);
+    });
+
+    const db = {};
+    db.sequelize = Sequelize;
+    db.sequelize = sequelize;
+
+    //connecting to model
+    db.users = require('./userModel') (sequelize, DataTypes);
